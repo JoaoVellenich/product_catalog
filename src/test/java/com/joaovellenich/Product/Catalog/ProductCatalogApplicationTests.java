4a5a79c441ab31e23afc9bf06b0b1a2b2e63c0b4
JoaoVellenich <jpvellenich@gmail.com>
2024-02-26 18:22:24 -0300

    feat: start

---
+++ src/test/java/com/joaovellenich/Product/Catalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovellenich.Product.Catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
